f1e99f79e42685a55a8ac52c71bbbe132b03895e
Lin Dong <ldong@gatech.edu>
2015-01-25 21:15:45 -0800

    Initial Commit

---
+++ SnipperView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnipperView
+//
+//  Created by Lin Dong on 1/25/15.
+//  Copyright (c) 2015 Lin Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
